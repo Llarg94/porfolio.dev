404f4c83743a307a82c2a7d8334d3638468b4ddd
Albert <albertllarg1@gmail.com>
2024-10-31 23:23:24 +0100

    Muchos cambios...     - Nuevo Header     - ToogleBar     - iconos     - SocialPill Modificado     - Encontrado el error de por qué no funcionaban los hovers

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}", // Incluye todas las rutas necesarias
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}", // Incluye todas las rutas necesariasç
+    "./components/**/*.{astro,js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
